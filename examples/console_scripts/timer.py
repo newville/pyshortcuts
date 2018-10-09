c1b03b6548745390941b31364e4c4d77906337b6
Matthew Newville <newville@cars.uchicago.edu>
2018-10-09 11:57:04 -0500

    add optional arguments to timer.py example script

--- examples/console_scripts/timer.py
+++ examples/console_scripts/timer.py
@@ -1,7 +1,32 @@
-from time import sleep
+from time import sleep, time
 from datetime import datetime
 
+from optparse import OptionParser
+
+usage = "Usage: timer.py [options]"
+
+parser = OptionParser(usage=usage, prog="timer", version="simple clock")
+
+parser.add_option("-t", "--time", dest="maxtime", metavar='maxtime',
+                      default=None, help="maximum run time (seconds)")
+parser.add_option("-u", "--update", dest="update", metavar='update',
+                      default=0.1, help="update time (seconds)")
+
+(options, args) = parser.parse_args()
+
+update = float(options.update)
+maxtime = options.maxtime
+if maxtime is not None:
+    maxtime = float(maxtime)
+
+t0 = time()
 
 while True:
-    sleep(0.25)
-    print(datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'))
+    sleep(update)
+    print("%s : %.3f " % (datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'), time()-t0))
+
+    if maxtime is not None and (time()-t0 > maxtime):
+        break
+
+print("done.")
+sleep(0.5)
