85097901d3a4958ffd4311459500bec9dc45f82e
Matt Newville <newville@cars.uchicago.edu>
2018-10-10 14:50:43 -0500

    fiixes for windows, especially for adding arguments to a script

--- pyshortcuts/windows.py
+++ pyshortcuts/windows.py
@@ -17,35 +17,27 @@ def make_shortcut(script, name, description=None, terminal=False,
         description = name
 
     homedir = get_homedir()
-    desktop, target, icon_path = get_paths(script, icon_path)
+    desktop, sname, icon_path = get_paths(script, icon_path)
 
     pyexe = os.path.join(sys.prefix, 'pythonw.exe')
     if terminal:
         pyexe = os.path.join(sys.prefix, 'python.exe')
 
-    # add several checks for valid ways to run each script, including
-    # accounting for Anaconda's automagic renaming and creation of exes.
-    target_exe = '%s.exe' % target
-    target_bat = '%s.bat' % target
-    target_spy = '%s-script.py' % target
-
-    if os.path.exists(target_exe):
-        target = target_exe
-    elif os.path.exists(target_spy):
-        target = "%s %s" % (pyexe, target_spy)
-    elif os.path.exists(target):
-        fbat = open(target_bat, 'w')
-        fbat.write("""@echo off
-
-%s %%~dp0%%~n0 %%1 %%2 %%3 %%4 %%5
-
-        """ % (pyexe))
-        fbat.close()
-        target = target_bat
+    # check for other valid ways to run each script, allowing
+    # for Anaconda's automagic renaming and creation of exes.
+    args = sname.split()
+    sname = args.pop(0)
+    if not os.path.exists(sname):
+        for suffix in ('.exe', '-script.py'):
+            tname = sname + suffix
+            if os.path.exists(tname):
+                sname = tname
+    args.insert(0, sname)
 
     shortcut = Dispatch('WScript.Shell').CreateShortCut(
         os.path.join(desktop, name) +  '.lnk')
-    shortcut.Targetpath = target
+    shortcut.Targetpath = '"%s"' % pyexe
+    shortcut.Arguments = ' '.join(args)
     shortcut.WorkingDirectory = homedir
     shortcut.WindowStyle = 0
     shortcut.Description = description
