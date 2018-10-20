861e9916a5f07c3f50a5934cde45d52bd178780e
Matthew Newville <newville@cars.uchicago.edu>
2018-10-19 21:28:20 -0500

    version to 1.1

--- pyshortcuts/__init__.py
+++ pyshortcuts/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-__version__ = '1.0'
+__version__ = '1.1'
 
 import os
 import sys
--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ if sys.platform.startswith('win'):
     install_reqs.append['pywin32']
 
 setup(name='pyshortcuts',
-      version='1.0',
+      version='1.1',
       author='Matthew Newville',
       author_email='newville@cars.uchicago.edu',
       url='http://github.com/newville/pyshortcuts',
