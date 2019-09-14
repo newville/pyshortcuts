d5f6d3c95ca604dec294c81e949f7cfe4aed1695
matt wilkie <maphew@gmail.com>
2019-09-14 11:35:43 -0700

    clean up: remove dead code, add docs, consistent quotes
    I used _black_ for reformatting, which uses " " instead of ' '.
    @newville this can be reverted if you don't like the convention.

--- pyshortcuts/utils.py
+++ pyshortcuts/utils.py
@@ -1,33 +1,36 @@
 #!/usr/bin/env python
 """
 utilities for pyshortcuts
-  get_homedir()  get home directory
-  get_paths()
-
+  get_homedir()         get home directory (all platforms)
+  get_win_folders()     get Home, Desktop and Startmenu paths
+  fix_paths()           get absolute paths for script, target folder, icon
 """
 import os
 import sys
 
 
 platform = sys.platform
-if os.name == 'nt':
-    platform = 'win'
-if platform == 'linux2':
-    platform = 'linux'
+if os.name == "nt":
+    platform = "win"
+if platform == "linux2":
+    platform = "linux"
+
 
 def unixpath(d):
     "unix path"
-    return d.replace('\\', '/')
+    return d.replace("\\", "/")
+
 
 def winpath(d):
     "ensure path uses windows delimiters"
-    if d.startswith('//'):
+    if d.startswith("//"):
         d = d[1:]
-    d = d.replace('/', '\\')
+    d = d.replace("/", "\\")
     return d
 
+
 nativepath = unixpath
-if platform == 'win':
+if platform == "win":
     nativepath = winpath
 
 HAS_PWD = True
@@ -38,19 +41,23 @@ except ImportError:
 
 ## mhw: WIP: getting a handle on named tuples
 def get_win_folders():
-    '''Return named tuple of Home, Desktop and Startmenu paths.
+    """Return named tuple of Home, Desktop and Startmenu paths.
 
         folders = get_win_folders()
         print("Home, Desktop, StartMenu ",
             folders.home, folders.desktop, folders.startmenu)
-    '''
+    """
     import win32com.client
+
     shellapp = win32com.client.Dispatch("Shell.Application")
     from collections import namedtuple
-    nt = namedtuple('folders', 'home desktop startmenu')
-    folders = nt(os.environ['USERPROFILE'],
-                shellapp.namespace(11).self.path,
-                shellapp.namespace(0).self.path)
+
+    nt = namedtuple("folders", "home desktop startmenu")
+    folders = nt(
+        os.environ["USERPROFILE"],
+        shellapp.namespace(11).self.path,
+        shellapp.namespace(0).self.path,
+    )
     return folders
     # Windows Special Folders
     # ID numbers from https://gist.github.com/maphew/47e67b6a99e240f01aced8b6b5678eeb
@@ -59,30 +66,20 @@ def get_win_folders():
     # Start menu: user = 11, all users = 22
     # Desktop   : user =  0, all users = 25
 
+
 ##
+
 
 def get_homedir():
     "determine home directory of current user"
     home = None
 
     def get_home_from_env():
-    # '~' -- should work on most Unixes
-    # then try common environmental variables
-        for var in ('~', '$HOME', '$HOMEPATH', '$USERPROFILE', '$ALLUSERSPROFILE'):
+        # try '~' which works on most Unixes, then common environmental variables
+        for var in ("~", "$HOME", "$HOMEPATH", "$USERPROFILE", "$ALLUSERSPROFILE"):
             p = os.path.expandvars(var)
             if os.path.exists(p):
                 return p
-
-    ## Verifies string coming out of expandvar doesn't match the var name
-    ## so doesn't provide much protection, the dir could still not exist
-    # def check(method, s):
-    #     "check that os.path.expanduser / expandvars gives a useful result"
-    #     try:
-    #         if method(s) not in (None, s):
-    #             return method(s)
-    #     except:
-    #         pass
-    #     return None
 
     # for Unixes, allow for sudo case
     susername = os.environ.get("SUDO_USER", None)
@@ -94,7 +91,7 @@ def get_homedir():
 
     # finally, use current folder
     else:
-        home = os.path.abspath('.')
+        home = os.path.abspath(".")
 
     return nativepath(home)
 
@@ -109,7 +106,7 @@ def fix_paths(script, folder=None, icon_path=None, icon=None):
     """
     scriptname = os.path.abspath(script)
 
-    dest = os.path.join(get_homedir(), 'Desktop')
+    dest = os.path.join(get_homedir(), "Desktop")
     if folder is not None:
         dest = os.path.join(dest, folder)
         if not os.path.exists(dest):
@@ -117,12 +114,12 @@ def fix_paths(script, folder=None, icon_path=None, icon=None):
 
     if icon_path is None:
         _path, _fname = os.path.split(__file__)
-        icon_path = os.path.join(_path, 'icons')
+        icon_path = os.path.join(_path, "icons")
     if icon is None:
-        icon = 'py'
-    ext = '.ico'
-    if platform.startswith('darwin'):
-        ext = '.icns'
+        icon = "py"
+    ext = ".ico"
+    if platform.startswith("darwin"):
+        ext = ".icns"
     iconfile = os.path.abspath(os.path.join(icon_path, icon + ext))
 
     return scriptname, dest, iconfile
