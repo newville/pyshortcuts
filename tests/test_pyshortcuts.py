88878d4cbbf0bbd43ab259ecf0bdd5584e445f27
matt wilkie <maphew@gmail.com>
2019-10-30 22:12:06 -0700

    testing if previous commit fixes #19 (answer: No)
    the commit correctly emits error if `cmd = None`, but is silent if it
    is empty string `cmd = ""`

--- tests/test_pyshortcuts.py
+++ tests/test_pyshortcuts.py
@@ -7,7 +7,8 @@ root = os.path.abspath(os.path.join(__file__, '..', '..'))
 folders = get_folders()
 
 def test_shortcut():
-    print("---folders:\n Home: {}\n Desktop: {}\n StartMenu: {}\n".format(
+    print('---test_shortcut()')
+    print("folders:\n Home: {}\n Desktop: {}\n StartMenu: {}\n".format(
         folders.home, folders.desktop, folders.startmenu))
 
     script = os.path.join(root, 'examples', 'console_scripts', 'timer.py') + ' -u 0.25 -t 10'
@@ -23,5 +24,24 @@ def test_shortcut():
 
     #raise err # force crash so we see manual print() debug statements
 
+def test_empty_string_script():
+    '''Do we fail to: "AttributeError crash if shortcut cmd is undefined #19" ?'''
+    print('---test_empty_script()')
+    cmd = ""
+    scut = make_shortcut(cmd, name="My Script is empty")
+    print('*** if you see this line and no error then test failed\n')
+    assert isinstance(scut, Shortcut), 'it returns a shortcut instance'
+
+def test_none_script():
+    '''Do we fail to: "AttributeError crash if shortcut cmd is undefined #19" ?'''
+    print('---test_none_script()')
+    cmd = None
+    scut = make_shortcut(cmd, name="My Script is empty")
+    print('*** if you see this line and no error then test failed\n')
+    assert isinstance(scut, Shortcut), 'it returns a shortcut instance'
+
+
 if __name__ == '__main__':
     test_shortcut()
+    test_empty_string_script()
+    test_none_script()
